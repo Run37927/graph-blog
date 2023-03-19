b2a6c9fd9027781553ff45520b36189c90a076b9
Hairun Huang <hairunhuang@gmail.com>
2023-03-19 13:08:23 -0700

    me query to see who i am

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,14 @@
 import { Context } from "..";
 
 export const Query = {
+  me: (parent: any, args: any, { userInfo, prisma }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
   posts: (parent: any, args: any, { prisma }: Context) => {
     return prisma.post.findMany({
       orderBy: [
